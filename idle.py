ca7797c09cb3f59a4c9f9b344b47beb4d13d1576
Zoroxus <maxime.pouleyn@epitech.eu>
2023-09-13 17:02:43 +0200

    create first steps of idle game

---
+++ idle.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+import time
+import PySimpleGUI as sg
+
+
+"""
+    Demo Program - Multithreaded Long Tasks GUI
+    
+    Presents one method for running long-running operations in a PySimpleGUI environment.
+    
+    The PySimpleGUI code, and thus the underlying GUI framework, runs as the primary, main thread
+    The "long work" is contained in the thread that is being started.
+
+    So that you don't have to import and understand the threading module, this program uses window.start_thread to run a thread.
+    
+    The thread is using TUPLES for its keys.  This enables you to easily find the thread events by looking at event[0].
+        The Thread Keys look something like this:  ('-THREAD-', message)
+        If event [0] == '-THREAD-' then you know it's one of these tuple keys.
+         
+    Copyright 2022 PySimpleGUI
+"""
+points = 0
+build1 = 0
+buildcost1 = 10
+
+def long_operation_thread(window):
+    """
+    A worker thread that communicates with the GUI through a queue
+    This thread can block for as long as it wants and the GUI will not be affected
+    :param seconds: (int) How long to sleep, the ultimate blocking call
+    :param window: (sg.Window) the window to communicate with
+    :return:
+    """
+    global points
+    global added
+    global build1
+    global buildcost1
+    while True:
+        added=build1
+        points+=added
+        time.sleep(1)
+
+
+def the_gui():
+    """
+    Starts and executes the GUI
+    Reads data from a Queue and displays the data to the window
+    Returns when the user exits / closes the window
+    """
+    global points
+    global build1
+    global buildcost1
+
+    sg.theme('Light Brown 3')
+
+    layout = [[sg.Text('0', key="-POINTS-")],
+              [sg.Button("CLICK")],
+              [sg.Text("Building 1  |"),sg.Text(buildcost1, key="-TEXT1-")],
+              [sg.Text('0', key="-BUILD1-")],
+              [sg.Button("BUY")],
+              [sg.Button('Click Me I Do Nothing !'), sg.Button('Exit')],
+    ]
+
+    window = sg.Window('Multithreaded Window', layout)
+
+    window.start_thread(lambda: long_operation_thread(window), ('-THREAD-', '-THEAD ENDED-'))
+    current_time = 0
+    paused = False
+    start_time = int(round(time.time() * 100))
+    # --------------------- EVENT LOOP ---------------------
+    while True:
+        # --------- Read and update window --------
+        event, values = window.read(timeout=10)
+        current_time = int(round(time.time() * 100)) - start_time
+        print(event, values, points)
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+        if event == 'CLICK':
+            points+=1
+        if event == 'BUY':
+            if points>=buildcost1:
+                build1+=1
+                points-=buildcost1
+                buildcost1+=build1*2
+                window["-TEXT1-"].update(buildcost1)
+        bct1=str(buildcost1)
+        #window["-TEXT1-"].update('Building 1 | Cost : ',bct1)
+        window["-BUILD1-"].update(build1)
+        # --------- Display timer in window --------
+        window['-POINTS-'].update(points)
+
+
+    # if user exits the window, then close the window and exit the GUI func
+    window.close()
+
+if __name__ == '__main__':
+    the_gui()
+    print('Exiting Program')
